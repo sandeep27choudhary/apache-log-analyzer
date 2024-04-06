1301212cb4b9d20b6ffa934ec9cb3519aeedef28
SANDEEP KUMAR CHOUDHARY <75579219+sandeep27choudhary@users.noreply.github.com>
2024-04-06 07:27:35 +0530

    corrected and added debugs

--- ansible-log-analyzer/ansible-playbook/server_log_analyzer.sh
+++ ansible-log-analyzer/ansible-playbook/server_log_analyzer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#check if the log file exists or not
 
-if [[ ! -f $0 ]]; then
-    echo "Error: Log file not found: $0"
+# Check if log file argument is provided
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 <log_file>"
+    exit 1
+fi
+
+#check if the log file exists or not
+if [[ ! -f $1 ]]; then
+    echo "Error: Log file not found: $1"
     exit 1
 fi
